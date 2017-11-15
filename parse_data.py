61bdce3e83f1f508648c1b02a45f8d8fac1d09b4
Morten Mjelva <morten.mjelva@gmail.com>
2017-11-15 15:22:27 +0100

    Finished data parsing

--- parse_data.py
+++ parse_data.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import xarray as xr
 import scipy.interpolate
 
-def interpolate_points(grid_x, grid_y, x, y, values, method='linear'):
+
+def interpolate_points(grid_x, grid_y, x, y, values, step, method='linear'):
     """
     Interpolate points into DataArray
-    
+
     grid_x: X axis of the grid
     grid_y: Y axis of the grid
     x: X value of each point
@@ -16,46 +18,51 @@ def interpolate_points(grid_x, grid_y, x, y, values, method='linear'):
     values: Value at each point
     method: scipy.interpolate.griddata method to use
     """
+    def _interpolate(value):
+        return scipy.interpolate.griddata(
+                (x, y),
+                value,
+                (grid_x, grid_y),
+                method=method)
 
-    array = scipy.interpolate.griddata(
-            (x, y),
-            values,
-            (grid_x, grid_y),
-            method=method)
+    data = {key: (['x', 'y'], _interpolate(values[key])) for key in values}
+    coords = {
+            'xc': (['x', 'y'], grid_x),
+            'yc': (['x', 'y'], grid_y),
+            'step': step}
 
-    darray = xr.DataArray(
-            array,
-            coords=[x, y],
-            dims=['x', 'y'])
-
-    return darray
+    ds = xr.Dataset(data, coords=coords)
+    return ds
 
 
 def main():
-    dtype = [
-            ('x', np.float64),
-            ('y', np.float64),
-            ('ux', np.float64),
-            ('uy', np.float64)
-        ]
+    ds = None
 
-    with open('data.txt') as f:
-        data = np.loadtxt(f, dtype=dtype)
+    xls = pd.ExcelFile('steps_result.xlsx')
+    for sheet in sorted(xls.sheet_names, key=lambda x: int(x.replace("Step_",""))):
+        df = xls.parse(sheet)
+        df = df.drop_duplicates()
 
-    data = np.unique(data)
-    x = data['x']
-    y = data['y']
-    ux = data['ux']
-    uy = data['uy']
+        step = int(sheet.replace("Step_", ""))
+        x = df['X']
+        y = df['Y']
+        ux = df['Ux']
+        uy = df['Uy']
 
-    X, Y = np.meshgrid(x, y)
-    da = interpolate_points(X, Y, x, y, uy)
+        X, Y = np.meshgrid(x, y)
+
+        values = {'ux': ux, 'uy': uy}
+        tmp_ds = interpolate_points(X, Y, x, y, values, step)
+
+        if ds is None:
+            ds = tmp_ds
+        else:
+            ds = xr.concat([ds, tmp_ds], 'step')
+
+    print(ds)
 
     plt.figure()
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.contourf(X, Y, da, origin='lower')
-    plt.colorbar()
-    plt.scatter(x, y, marker='o', c='b', s=5)
+    ds.uy[50].plot.pcolormesh(x='xc', y='yc', add_colorbar=True)
     plt.show()
 
 
